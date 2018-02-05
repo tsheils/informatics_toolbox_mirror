30fabe83b9bb9e3c9081b37614766d8b19fc64e6
Tim Sheils <timothy.sheils@nih.gov>
2018-02-05 14:54:47 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from '../assets/material/material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {NcatsHeaderComponent} from './ncats-header/ncats-header.component';
+import {NcatsFooterComponent} from './ncats-footer/ncats-footer.component';
+import {LoadingService} from './services/loading.service';
+import {DataConnectionService} from './services/data-connection.service';
+import {WebSocketService} from './services/websocket.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NcatsHeaderComponent,
+    NcatsFooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  providers: [
+    WebSocketService,
+    DataConnectionService,
+    LoadingService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
