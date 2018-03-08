9f9d5a38fceb7016daa392759cef4347210c1ad1
Tim Sheils <timothy.sheils@nih.gov>
2018-03-08 11:38:05 -0500

    merge changes

---
+++ src/app/services/data-search.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {DataLoaderService} from "./data-loader.service";
+import {Subject} from "rxjs/Subject";
+
+@Injectable()
+export class DataSearchService {
+    private _dataSource = new Subject<any>();
+    data$ = this._dataSource.asObservable();
+
+    constructor(private dataLoaderService: DataLoaderService) {
+        // initial passing of data
+        this.dataLoaderService.data$.subscribe(res => {
+         /*   this.masterDataMap = res;
+            this.years.forEach(year => {
+                this.returnedDataMap.set(year, this.masterDataMap.get(year));
+            });
+            this._dataSource.next({data: this.returnedDataMap});*/
+        });
+    }
+
+}
