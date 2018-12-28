125d44e9130b064d125cb00c590cda2de0dc9409
Jorge Neyra <jorge.neyra@nih.gov>
2018-12-27 21:31:06 -0500

    updated ux, grouped options on categories, and added previously used options

--- src/app/pages/resolver/resolver.component.ts
+++ src/app/pages/resolver/resolver.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChil
 import {FormControl} from '@angular/forms';
 import {Tool} from '../../models/tool';
 import {ResolverService} from './services/resolver.service';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource, MatExpansionModule} from '@angular/material';
 import {ActivatedRoute} from '@angular/router';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
@@ -30,15 +30,92 @@ export class ResolverComponent implements OnInit, AfterViewInit, OnDestroy {
     rawData: string;
     fields: string[];
     options: Option[];
+    categories: { [category: string]: Array<Option> } = {};
+    priorityOptions: { [category: string]: number} = {
+        'qhts': 3,
+        'smiles': 2,
+        'lychi': 1,
+    };
+    previouslyUsedOptions: { [category: string]: number} = {};
+    lastUsedOptions: { [category: string]: number} = {};
+    categoryNames: Array<string> = [];
     dataSource = new MatTableDataSource<any[]>([]);
     private ngUnsubscribe: Subject<any> = new Subject();
 
 
-    constructor(private resolverService: ResolverService) {}
+    constructor(
+        private resolverService: ResolverService
+    ) {}
     ngOnInit() {
+        this.previouslyUsedOptions = JSON.parse(localStorage.getItem('previouslyUsedOptions')) || {};
+        this.lastUsedOptions = JSON.parse(localStorage.getItem('lastUsedOptions')) || {};
+        let priorityOptionsCategoryName = 'Popular';
+
+        if (Object.keys(this.lastUsedOptions).length > 0 || Object.keys(this.previouslyUsedOptions).length > 0) {
+            priorityOptionsCategoryName = 'Previously used';
+            this.priorityOptions = {};
+
+            Object.keys(this.lastUsedOptions).forEach(key => {
+                this.priorityOptions[key] = this.lastUsedOptions[key];
+                this.properties.push(key);
+            });
+
+            Object.keys(this.previouslyUsedOptions).forEach((key) => {
+                if (this.priorityOptions[key] == null && Object.keys(this.priorityOptions).length < 6) {
+                    this.priorityOptions[key] = this.previouslyUsedOptions[key];
+                }
+            });
+        }
+
         this.resolverService.getOptions()
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe(res => {
+                res.forEach(option => {
+
+                    if (this.priorityOptions[option.name] != null) {
+                        if (this.categories[priorityOptionsCategoryName] == null) {
+                            this.categories[priorityOptionsCategoryName] = [];
+                            this.categoryNames.unshift(priorityOptionsCategoryName);
+                        }
+
+                        if (this.categories[priorityOptionsCategoryName].length === 0
+                            || this.priorityOptions[option.name]
+                            <= this.priorityOptions[this.categories[priorityOptionsCategoryName]
+                            [this.categories[priorityOptionsCategoryName].length - 1].name]) {
+                            this.categories[priorityOptionsCategoryName].push(option);
+                        } else {
+                            for (let i = 0; i < this.categories[priorityOptionsCategoryName].length; i++) {
+                                if (this.priorityOptions[option.name] > this.priorityOptions[this.categories
+                                    [priorityOptionsCategoryName][i].name]) {
+                                    this.categories[priorityOptionsCategoryName].splice(i, 0, option);
+                                    break;
+                                }
+                            }
+                        }
+                    } else if (option.tags != null && option.tags.length) {
+                        option.tags.forEach(tag => {
+                            if (tag.indexOf('category-') > -1) {
+                                const category = tag.split('category-').pop();
+                                if (this.categories[category] == null) {
+                                    this.categories[category] = [];
+                                    this.categoryNames.push(category);
+                                }
+
+                                if (this.categories[category].length === 0
+                                    || option.title >= this.categories[category][this.categories[category].length - 1].title) {
+                                    this.categories[category].push(option);
+                                } else {
+                                    for (let i = 0; i < this.categories[category].length; i++) {
+                                        if (option.title < this.categories[category][i].title) {
+                                            this.categories[category].splice(i, 0, option);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
                 this.options = res;
             });
 
@@ -73,6 +150,16 @@ export class ResolverComponent implements OnInit, AfterViewInit, OnDestroy {
             this.rawData =  lines.join('\n');
             this.loaded = true;
         });
+        this.lastUsedOptions = {};
+        this.properties.forEach(property => {
+            if (this.previouslyUsedOptions[property] == null) {
+                this.previouslyUsedOptions[property] = 0;
+            }
+            this.previouslyUsedOptions[property]++;
+            this.lastUsedOptions[property] = Number.MAX_SAFE_INTEGER;
+        });
+        localStorage.setItem('previouslyUsedOptions', JSON.stringify(this.previouslyUsedOptions));
+        localStorage.setItem('lastUsedOptions', JSON.stringify(this.lastUsedOptions));
     }
 
     checked(event: any, property: string) {
@@ -90,6 +177,10 @@ export class ResolverComponent implements OnInit, AfterViewInit, OnDestroy {
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+        setTimeout(() => {
+            const topElement = document.getElementsByClassName('directions-container')[0];
+            topElement.scrollIntoView(true);
+        });
     }
     applyFilter(filterValue: string) {
         filterValue = filterValue.trim(); // Remove whitespace
