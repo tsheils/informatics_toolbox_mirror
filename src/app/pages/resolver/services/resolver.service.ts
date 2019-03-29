08f5900fb4586c865dbee0cc476d248ba3c55753
Jorge Neyra <jorge.neyra@nih.gov>
2019-03-29 18:31:50 -0400

    fix for encoding issue

--- src/app/pages/resolver/services/resolver.service.ts
+++ src/app/pages/resolver/services/resolver.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {from, Observable, of, Subject} from 'rxjs';
-import {environment} from '../../../../environments/environment';
-import {map} from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from, Observable, of, Subject } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 import { Option } from '../option';
 
 // const URL = 'https://tripod.nih.gov/servlet/resolverBeta3/';
- const URL = 'https://tripod.nih.gov/servlet/resolverBeta4/';
+const URL = 'https://tripod.nih.gov/servlet/resolverBeta4/';
 const ENVIRONMENT = environment;
 const httpOptions = {
     headers: new HttpHeaders({
@@ -25,39 +25,46 @@ export class ResolverService {
     }
 
     getOptions(): Observable<any> {
-       return this.http.get<any>(URL + '_options').pipe(
+        return this.http.get<any>(URL + '_options').pipe(
             map(res => {
-            // if (ENVIRONMENT.public === true) {
-            //     const ret = res.filter(field => !field.tags.includes('restricted'));
-            //     return ret;
-            // } else {
-            //     return res;
-            // }
+                // if (ENVIRONMENT.public === true) {
+                //     const ret = res.filter(field => !field.tags.includes('restricted'));
+                //     return ret;
+                // } else {
+                //     return res;
+                // }
 
-            res = res.map(option => {
-                if ((ENVIRONMENT.public && !option.tags.includes('restricted')) || !ENVIRONMENT.public) {
-                    return new Option().fromJSON(option);
-                }
-            });
-            return res;
-        })
+                res = res.map(option => {
+                    if ((ENVIRONMENT.public && !option.tags.includes('restricted')) || !ENVIRONMENT.public) {
+                        return new Option().fromJSON(option);
+                    }
+                });
+                return res;
+            })
         );
     }
 
-    resolveData(parameters: string[], names: string[]): Observable<any> {
-      const url = URL + parameters.join('/');
-      const data: any = {
-          structure: names.map(name => name = this.parseSmiles(name)),
-          format: 'json'
-      };
+    resolveData(parameters: string[], names: Array<string>): Observable<any> {
+        console.log(names);
+        const url = URL + parameters.join('/');
+        const data: any = {
+            structure: names.join('\n'),
+            format: 'json'
+        };
         if (ENVIRONMENT.public === false) {
             data.apiKey = '5fd5bb2a05eb6195';
-            return this.http.post<any>(url, 'structure=' + names.join('%0A') + '&format=json&apikey=5fd5bb2a05eb6195', httpOptions);
-        } else {
-            return this.http.post<any>(url, 'structure=' + names.join('%0A') + '&format=json', httpOptions);
         }
+        return this.http.post<any>(url, this.toUrlEncodedParams(data), httpOptions);
     }
 
+    private toUrlEncodedParams(body: {[paramName: string]: any }): string {
+        let encodedParams = '';
+        Object.keys(body).forEach((key, index) => {
+            encodedParams +=
+                `${index !== 0 ? '&' : ''}${encodeURIComponent(key)}=${encodeURIComponent(body[key])}`;
+        });
+        return encodedParams;
+    }
 
     private parseSmiles(smiles: string): string {
         const parsed = smiles
